d754b522b9cf425965397ca0fafcde707deb53ca
Diogo Deconto <dideconto@gmail.com>
2024-04-01 20:20:18 -0300

    API - Cadastro de produto pelo corpo da requisição

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -4,36 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos =
-[
-    new Produto("Celular", "Android", 4500.5),
-    new Produto("Celular", "IOS", 3000),
-    new Produto("Televisão", "LG", 2000),
-    new Produto("Cafeteira", "Oaster", 250)
-];
-
-// Cadastrar um produto na lista
-// a) Através das informações na URL
-// b) Através das informações no corpo da requisição
-// Realizar as operações alteração e remoção da lista
+List<Produto> produtos = new List<Produto>();
 
 // Endpoints = Funcionalidades - JSON
 // POST: http://localhost:5076/api/produto/cadastrar
-app.MapPost("/api/produto/cadastrar/",
-    ([FromRoute] string nome, [FromRoute] string descricao) =>
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
 {
-    //Preenchendo o objeto pelo construtor
-    Produto produto = new Produto(nome, descricao, 123);
-
-    //Preenchendo o objeto pelo atributo
-    produto.Nome = nome;
-    produto.Descricao = descricao;
-
     //Adicionando o produto dentro da lista
     produtos.Add(produto);
-
     return Results.Created("", produto);
-
 });
 
 // GET: http://localhost:5076/api/produto/listar
