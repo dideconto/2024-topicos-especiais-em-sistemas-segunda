84cd5c9ec6559b092fce67f7233e104a81ff5ccc
jasobreiro <jasobreiro@up.edu.br>
2024-05-13 20:42:31 -0300

    FRONT - Projeto base em React

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Minha aplicação em React</h1>
+      <h2><code>e o cafézinho vai ta tendo?</code></h2>
+    </div>
+  );
+}
+
+export default App;
