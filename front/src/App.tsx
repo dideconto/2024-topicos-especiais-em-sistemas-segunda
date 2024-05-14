b39ec3ac0589f1b5a3cc8ebe37819b3aa16fdf1b
jasobreiro <jasobreiro@up.edu.br>
2024-05-13 21:41:33 -0300

    FRONT - Componente da soma com contador

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import Soma from "./Soma";
+
+//1 - Um componente SEMPRE deve começar com a primeira letra
+//maiúscula
+//2 - Todo componente DEVE ser uma função do JS
+//3 - Todo deve retornar apenas UM elemento HTML
 function App() {
   return (
-    <div className="App">
-      <h1>Minha aplicação em React</h1>
-      <h2><code>e o cafézinho vai ta tendo?</code></h2>
+    <div>
+      <Soma></Soma>
     </div>
   );
 }
-
+//4 - OBRIGATORIAMENTE o componente DEVE ser exportado
 export default App;
